09f6fbc406d8b2d29c7dd649f8315854dd555718
cshica <cshica@gmail.com>
2023-04-07 02:31:30 -0600

    Agregar archivos de proyecto.

---
+++ WebApi/Controllers/CrudController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getDatos")]
+        public IActionResult ListarDatos()
+        {
+            var persona = new { 
+            Nombre="Pepito",
+            Direccion="Las cinco esquinas"
+            };
+            return Ok(persona);
+        }
+    }
+}
